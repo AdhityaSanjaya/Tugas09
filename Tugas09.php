d7092269b944fff04be53190de42130c2903ea81
AdhityaSanjaya <fadiladt20@gmail.com>
2024-11-17 13:20:41 +0700

    Add files via upload

---
+++ Tugas09.php
@@ -0,0 +1,11 @@
+<?php
+
+$nim = "312310058";
+$nama = "FADILLA ADHITYA SANJAYA";
+$kelas = "TI23CA1";
+
+echo "<h1>Data Mahasiswa</h1>";
+echo "<p><strong>NIM:</strong> $nim</p>";
+echo "<p><strong>Nama:</strong> $nama</p>";
+echo "<p><strong>Kelas:</strong> $kelas</p>";
+?>
